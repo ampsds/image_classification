5e444ed39aaa4742dedd5243716ef82f2c63d02f
ampsds <107558541+ampsds@users.noreply.github.com>
2022-12-26 16:10:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,58 @@ model = models.densenet121(pretrained=True)               # Trained on 1000 clas
 #model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
 model.eval()                                              # Turns off autograd and
 
+img_class_map = None
+mapping_file_path = 'index_to_name.json'                  # Human-readable names for Imagenet classes
+if os.path.isfile(mapping_file_path):
+    with open (mapping_file_path) as f:
+        img_class_map = json.load(f)
+
+# Transform input into the form our model expects
+def transform_image(infile):
+    input_transforms = [transforms.Resize(255),           # We use multiple TorchVision transforms to ready the image
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],       # Standard normalization for ImageNet model input
+            [0.229, 0.224, 0.225])]
+    my_transforms = transforms.Compose(input_transforms)
+    image = Image.open(infile)                            # Open the image file
+    timg = my_transforms(image)                           # Transform PIL image to appropriately-shaped PyTorch tensor
+    timg.unsqueeze_(0)                                    # PyTorch models expect batched input; create a batch of 1
+    return timg
+
+# Get a prediction
+def get_prediction(input_tensor):
+    outputs = model.forward(input_tensor)                 # Get likelihoods for all ImageNet classes
+    _, y_hat = outputs.max(1)                             # Extract the most likely class
+    prediction = y_hat.item()                             # Extract the int value from the PyTorch tensor
+    return prediction
+
+# Make the prediction human-readable
+def render_prediction(prediction_idx):
+    stridx = str(prediction_idx)
+    class_name = 'Unknown'
+    if img_class_map is not None:
+        if stridx in img_class_map is not None:
+            class_name = img_class_map[stridx][1]
+    return prediction_idx, class_name
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+@app.after_request
+def after_request(response):
+  # response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file is not None:
+            input_tensor = transform_image(file)
+            prediction_idx = get_prediction(input_tensor)
+            class_id, class_name = render_prediction(prediction_idx)
+            return jsonify({'class_id': class_id, 'class_name': class_name})
