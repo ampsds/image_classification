b4aca9f8fce0130e5bf3dd112bfce732f6ca202f
ampsds <107558541+ampsds@users.noreply.github.com>
2022-12-26 15:52:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,25 +14,6 @@ model = models.densenet121(pretrained=True)               # Trained on 1000 clas
 #model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
 model.eval()                                              # Turns off autograd and
 
-img_class_map = None
-mapping_file_path = 'index_to_name.json'                  # Human-readable names for Imagenet classes
-if os.path.isfile(mapping_file_path):
-    with open (mapping_file_path) as f:
-        img_class_map = json.load(f)
-
-# Transform input into the form our model expects
-def transform_image(infile):
-    input_transforms = [transforms.Resize(255),           # We use multiple TorchVision transforms to ready the image
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406],       # Standard normalization for ImageNet model input
-            [0.229, 0.224, 0.225])]
-    my_transforms = transforms.Compose(input_transforms)
-    image = Image.open(infile)                            # Open the image file
-    timg = my_transforms(image)                           # Transform PIL image to appropriately-shaped PyTorch tensor
-    timg.unsqueeze_(0)                                    # PyTorch models expect batched input; create a batch of 1
-    return timg
-
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
