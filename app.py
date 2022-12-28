524a383e5e4b9d6a4339eb772d2e958cadaa3220
ampsds <107558541+ampsds@users.noreply.github.com>
2022-12-29 01:52:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import io
 import json
 import os
 
+import torch
+import torch.nn as nn
 import torchvision.models as models
 import torchvision.transforms as transforms
 from torchvision.models import resnet50, ResNet50_Weights
@@ -16,8 +18,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-#model = models.densenet121(pretrained=True)               # Trained on 1000 classes from ImageNet
-model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
+model = models.resnet18(pretrained=True)              # Trained on 1000 classes from ImageNet
+#model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
 model.eval()                                              # Turns off autograd and
 
 ##ラインボット
@@ -28,40 +30,26 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 ##推論
-img_class_map = None
-mapping_file_path = 'index_to_name.json'                  # Human-readable names for Imagenet classes
-if os.path.isfile(mapping_file_path):
-    with open (mapping_file_path) as f:
-        img_class_map = json.load(f)
+imagenet_class_index = json.load(open('index_to_name.json'))
 
-# Transform input into the form our model expects
-def transform_image(infile):
-    input_transforms = [transforms.Resize(255),           # We use multiple TorchVision transforms to ready the image
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406],       # Standard normalization for ImageNet model input
-            [0.229, 0.224, 0.225])]
-    my_transforms = transforms.Compose(input_transforms)
-    image = Image.open(infile)                            # Open the image file
-    timg = my_transforms(image)                           # Transform PIL image to appropriately-shaped PyTorch tensor
-    timg.unsqueeze_(0)                                    # PyTorch models expect batched input; create a batch of 1
-    return timg
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
 
-# Get a prediction
-def get_prediction(input_tensor):
-    outputs = model.forward(input_tensor)                 # Get likelihoods for all ImageNet classes
-    _, y_hat = outputs.max(1)                             # Extract the most likely class
-    prediction = y_hat.item()                             # Extract the int value from the PyTorch tensor
-    return prediction
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    predicted_idx = str(y_hat.item())
+    return imagenet_class_index[predicted_idx]
+    #return classes[y_hat.item()]
 
-# Make the prediction human-readable
-def render_prediction(prediction_idx):
-    stridx = str(prediction_idx)
-    class_name = 'Unknown'
-    if img_class_map is not None:
-        if stridx in img_class_map is not None:
-            class_name = img_class_map[stridx][1]
-    return prediction_idx, class_name
 
 @app.route('/', methods=['GET'])
 def index():
@@ -78,11 +66,10 @@ def after_request(response):
 def predict():
     if request.method == 'POST':
         file = request.files['file']
-        if file is not None:
-            input_tensor = transform_image(file)
-            prediction_idx = get_prediction(input_tensor)
-            class_id, class_name = render_prediction(prediction_idx)
-            return jsonify({'class_id': class_id, 'class_name': class_name})
+        img_bytes = file.read()
+        class_id, class_name = get_prediction(image_bytes=img_bytes)
+        #class_name = get_prediction(image_bytes=img_bytes)
+        return jsonify({'class_name': class_name})
 
 #LINE BOTウェブフック
 @app.route("/callback", methods=['POST'])
@@ -110,11 +97,8 @@ def handle_image(event):
     img = b""
     for chunk in content.iter_content():
         img += chunk
-        
-    input_tensor = transform_image(img)
-    prediction_idx = get_prediction(input_tensor)
-    class_id, class_name = render_prediction(prediction_idx) 
-
+ 
+    class_name = get_prediction(image_bytes=img)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=class_name))
